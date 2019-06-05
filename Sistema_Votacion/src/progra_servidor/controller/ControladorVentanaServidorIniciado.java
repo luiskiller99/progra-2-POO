0afac0f056de74f2284ce66d49f8eeb7cc7c3244
Isaac Ortega Arguedas <irsacfac@gmail.com>
2019-06-05 16:15:01 -0600

    Ventanas creadas
    Se modifico la ventana: VentanaInicioServer.
    Se agrego, al menos temporalmente, la ventana: ventanaServerIniciado

---
+++ Sistema_Votacion/src/progra_servidor/controller/ControladorVentanaServidorIniciado.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra_servidor.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author irsac
+ */
+public class ControladorVentanaServidorIniciado implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
