8f5ccb0072acd675718947d0044c836fc247ecc0
luiskiller99 <metal0500@gmail.com>
2019-06-05 18:27:13 -0600

    interfaz cliente sin errores

--- Sistema_Votacion/src/progra_cliente/controller/Controller_ventana_ingresar_cliente.java
+++ Sistema_Votacion/src/progra_cliente/controller/Controller_ventana_ingresar_cliente.java
@@ -6,7 +6,6 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
